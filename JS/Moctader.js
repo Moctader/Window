6a929729735df1a275e54d1abdba4dca74f40273
Moctader <moctader.golam@gmail.com>
2021-02-07 11:22:17 +0200

    test

--- JS/Moctader.js
+++ JS/Moctader.js
@@ -9,7 +9,7 @@ function window_calculation() {
 
     let message = "";
 
-    if (windows_height <= 200 && window_width <= 200){
+    if (windows_height <= 200 && window_width <= 200 && window_amount <= 10){
         let wood_needed = ((windows_height * window_width * 6)/10000)
         let glass_needed = ((windows_height * window_width * 1)/10000)
 
@@ -24,7 +24,8 @@ function window_calculation() {
         document.getElementById("woods").innerHTML = Set_wood;
         document.getElementById("glasses").innerHTML = Set_glass;
 
-        document.getElementById("size").innerHTML = "large window";
+
+
         
 
     } else {
@@ -33,19 +34,60 @@ function window_calculation() {
 
     }
     
-    /*
     
-    if (windows_height <= 50 && window_width <= 50 && window_amount >=12){
-        message ="Minimum height and width of window is 50cm."
- 
-    } else if (windows_height <= 100 && window_width <= 100 && window_amount <=12) {
-        message = "No more than 10 equal size windows."
+    if (windows_height >= 50 && window_width >= 50 && window_amount <= 10){
+        let wood_needed = ((windows_height * window_width * 6)/10000)
+        let glass_needed = ((windows_height * window_width * 1)/10000)
 
-    } else {
-        message = "No amount. 1 assumed."
+
+        let Set_wood = (wood_needed * window_amount)
+        let Set_glass = (glass_needed * window_amount)
+
+
+        document.getElementById("wood").innerHTML = wood_needed;
+        document.getElementById("glass").innerHTML = glass_needed;
+
+        document.getElementById("woods").innerHTML = Set_wood;
+        document.getElementById("glasses").innerHTML = Set_glass;
+
+
+    }else {
+        message ="Minimum height and width of window is 50cm."
+        document.getElementById("error").innerHTML = message;
+
 
     }
-    document.getElementById("error").innerHTML = message;
 
-*/
+ 
+    if (window_amount <=10) {
+
+       // document.getElementById("error").innerHTML = window_amount;
+    } else {
+        message = "No more than 10 equal size windows."
+        document.getElementById("error").innerHTML = message;
+
+    }
+
+    if (windows_height <= 200 && window_width <= 200 && window_amount === null) {
+        message = "No amount. 1 assumed"
+        document.getElementById("error").innerHTML = message;
+    }
+
+
+    if(glass_needed <= 0.5){
+
+        document.getElementById("size").innerHTML = "small window";
+
+    } else if (glass_needed >= 0.6 && glass_needed <= 1.5){
+
+        document.getElementById("size").innerHTML = "medium window";
+
+    } else if (glass_needed >= 1.6 && glass_needed <= 2.5) {
+
+        document.getElementById("size").innerHTML = "medium window";
+
+    } else{
+        document.getElementById("size").innerHTML = "huge window";
+
+    }
 }
